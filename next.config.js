5909252f1622dd15b9d92b7112a9581360c15a26
Simon Fan <simon@orioro.design>
2023-10-03 03:10:34 -0300

    fix: prepare deploy to netlify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
-  basePath: '/redus-geo-poc',
-  assetPrefix: '/redus-geo-poc/', // assetPrefix requires the trailing slas
 }
 
 module.exports = nextConfig
