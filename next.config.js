062d8c8d26f2eb9aaa7044697274c1cd1c296e55
Simon Fan <simon@orioro.design>
2023-10-03 02:36:56 -0300

    fix: add next.config.js basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  basePath: '/redus-geo-poc',
+  assetPrefix: '/redus-geo-poc/', // assetPrefix requires the trailing slas
 }
 
 module.exports = nextConfig
